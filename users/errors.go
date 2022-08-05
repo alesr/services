0af71a2dfaf831dff0abdf5c80d34e2946a81b6c
Alessandro Resta <alessandro.resta@proton.me>
2022-08-05 21:06:41 +0200

    Fix typo

--- users/errors.go
+++ users/errors.go
@@ -19,12 +19,12 @@ func (e E) Error() string {
 var (
 	// Enumerate service errors
 
-	errAlreadyExists     = newE("user already exists")
-	errNotFound          = newE("user not found")
-	errPasswordMissmatch = newE("user password missmatch")
-	errRoleInvalid       = newE("user role is invalid")
-	errTokenEmpty        = newE("user token is empty")
-	errTokenExpired      = newE("user token is expired")
-	errTokenInvalid      = newE("user token is invalid")
-	errPasswordInvalid   = newE("user password is invalid")
+	errAlreadyExists    = newE("user already exists")
+	errNotFound         = newE("user not found")
+	errPasswordMismatch = newE("user password mismatch")
+	errRoleInvalid      = newE("user role is invalid")
+	errTokenEmpty       = newE("user token is empty")
+	errTokenExpired     = newE("user token is expired")
+	errTokenInvalid     = newE("user token is invalid")
+	errPasswordInvalid  = newE("user password is invalid")
 )
