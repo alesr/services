8569cf3ab0cc349b83df797ba836a8edcc49d320
Alessandro Resta <alessandro.resta@proton.me>
2022-08-06 14:22:40 +0200

    Refactor postgres implementation and add integration tests

--- migrations/1_create_users_table.up.sql
+++ migrations/1_create_users_table.up.sql
@@ -4,12 +4,12 @@ CREATE TABLE IF NOT EXISTS users (
     id UUID PRIMARY KEY,
     fullname VARCHAR(255) NOT NULL,
     username VARCHAR(255) NOT NULL,
-    birthdate DATE NOT NULL,
+    birthdate VARCHAR(10) NOT NULL,
     email VARCHAR(255) NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
     role role NOT NULL,
-    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 CREATE INDEX ON users(id);
